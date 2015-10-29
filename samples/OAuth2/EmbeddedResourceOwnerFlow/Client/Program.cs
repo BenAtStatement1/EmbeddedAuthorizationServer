eb3b4c2df1cdbe8264c121193b934920cf143bf4
benatstatement1 <ben@statement1.com>
2015-10-29 11:46:31 -0400

    .

--- samples/OAuth2/EmbeddedResourceOwnerFlow/Client/Program.cs
+++ samples/OAuth2/EmbeddedResourceOwnerFlow/Client/Program.cs
@@ -9,26 +9,38 @@ namespace Client
         static void Main(string[] args)
         {
             TokenResponse tok = GetToken();
-            Console.WriteLine("token:" + tok.AccessToken);
-            CallService(tok.AccessToken);
+            if (tok != null)
+            {
+                Console.WriteLine("token endpoint:" + tok.AccessToken);
+                var response=CallService(tok.AccessToken);
+                Console.WriteLine("identity endpoint:" + response);
+                Console.ReadLine();
+            }
         }
 
         private static TokenResponse GetToken()
         {
-            var client = new OAuth2Client(
+            try
+            {
+                var client = new OAuth2Client(
                 new Uri("http://localhost/token"));
 
-            return client.RequestResourceOwnerPasswordAsync("bob", "bob").Result;
+                return client.RequestResourceOwnerPasswordAsync("bob", "bob").Result;
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+            }
+            return null;
         }
 
-        private static void CallService(string token)
+        private static string CallService(string token)
         {
             var client = new HttpClient();
             client.SetBearerToken(token);
             var response = client.GetStringAsync(new Uri("http://localhost/api/identity")).Result;
 
-            Console.WriteLine(response);
-            Console.ReadLine();
+            return response;
         }
     }
 }
--- samples/OAuth2/EmbeddedResourceOwnerFlow/EmbeddedAuthorizationServer/Startup.cs
+++ samples/OAuth2/EmbeddedResourceOwnerFlow/EmbeddedAuthorizationServer/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using EmbeddedAuthorizationServer.Provider;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
@@ -13,21 +14,67 @@ namespace EmbeddedAuthorizationServer
         public void Configuration(IAppBuilder app)
         {
             // token generation
-            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
-                {
-                    // for demo purposes
-                    AllowInsecureHttp = true,
-
-                    TokenEndpointPath = new PathString("/token"),
-                    AccessTokenExpireTimeSpan = TimeSpan.FromHours(8),
-
-                    Provider = new SimpleAuthorizationServerProvider()
-                });
-
+            var serverOptions = new OAuthAuthorizationServerOptions
+            {
+                // for demo purposes
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(8),
+                Provider = new SimpleAuthorizationServerProvider(),
+                AuthenticationMode = AuthenticationMode.Active,
+                AuthenticationType = "Bearer"
+            };
+#if Debug
+            System.Diagnostics.Debugger.Launch();
+#endif
+            app.UseOAuthAuthorizationServer(serverOptions); // Adds OAuth2 Authorization Server capabilities to an OWIN web application.
+            /*  
+            This middleware performs the request processing for the Authorize and Token endpoints defined by the OAuth2 specification. 
+            See also http://tools.ietf.org/html/rfc6749
+            */
             // token consumption
-            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
-                
-            app.UseWebApi(WebApiConfig.Register());
+            var bearerOptions = new OAuthBearerAuthenticationOptions();
+            app.UseOAuthBearerAuthentication(bearerOptions); // Adds Bearer token processing to an OWIN application pipeline. 
+            /* 
+            This middleware understands appropriately formatted and 
+            secured tokens which appear in the request header. If the Options.AuthenticationMode is Active, the claims within the 
+            bearer token are added to the current request's IPrincipal User. 
+            See also http://tools.ietf.org/html/rfc6749 
+            */
+            app.UseWebApi(WebApiConfig.Register()); // add WebAPI endpoint processing to OWIN pipeline
         }
     }
 }
+
+/* 
+? serverOptions
+{Microsoft.Owin.Security.OAuth.OAuthAuthorizationServerOptions}
+    AccessTokenExpireTimeSpan: {08:00:00}
+    AccessTokenFormat: null
+    AccessTokenProvider: null
+    AllowInsecureHttp: true
+    ApplicationCanDisplayErrors: false
+    AuthenticationMode: Active
+    AuthenticationType: "Bearer"
+    AuthorizationCodeExpireTimeSpan: {00:05:00}
+    AuthorizationCodeFormat: null
+    AuthorizationCodeProvider: null
+    AuthorizeEndpointPath: {}
+    Description: {Microsoft.Owin.Security.AuthenticationDescription}
+    Provider: {EmbeddedAuthorizationServer.Provider.SimpleAuthorizationServerProvider}
+    RefreshTokenFormat: null
+    RefreshTokenProvider: null
+    SystemClock: {Microsoft.Owin.Infrastructure.SystemClock}
+    TokenEndpointPath: {/token}
+? bearerOptions
+{Microsoft.Owin.Security.OAuth.OAuthBearerAuthenticationOptions}
+    AccessTokenFormat: null
+    AccessTokenProvider: null
+    AuthenticationMode: Active
+    AuthenticationType: "Bearer"
+    Description: {Microsoft.Owin.Security.AuthenticationDescription}
+    Provider: null
+    Realm: null
+    SystemClock: {Microsoft.Owin.Infrastructure.SystemClock}
+    
+  */
